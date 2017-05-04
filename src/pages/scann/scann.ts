58d54b306bdee0ab48957e897a13a2538baf062b
Mark Caruso <kazoo6723@gmail.com>
2017-05-04 13:56:32 -0400

    Added getData() method to adjust length of barcode if needed. Added an if clause to scan.html to handle the case where data file cannot find the scanned barcode

--- src/pages/scann/scann.ts
+++ src/pages/scann/scann.ts
@@ -13,6 +13,8 @@ import {InAppBrowser } from '@ionic-native/in-app-browser';
 export class ScannPage {
 	private barcodeData;
 	data = [];
+	barcode:string;
+	foundIt:boolean;
 
   constructor(
     public navCtrl: NavController,
@@ -21,21 +23,36 @@ export class ScannPage {
     public platform: Platform,
     private iab: InAppBrowser) {
   	 this.barcodeData = this.navParams.get('details');
+  	 this.barcode = this.barcodeData.text;
+
   }
 
+  //make sure barcode is at least 13 characters log
+  getBarcode(){
+    console.log("Scanned Barcode: " + this.barcode);
+    console.log("Barcode length: " + this.barcode.length);
+    if(this.barcode.length < 13){
+      this.barcode = '0' + this.barcode;
+    }
+    console.log("Barcode length after if: " + this.barcode.length);
+    console.log("Scanned Barcode: " + this.barcode);
+    return this.barcode;
+  }
   launchUrl(url){
     this.platform.ready().then(() => {
       this.iab.create( url, "_system","location=yes" );
     })
   }
   ngOnInit(){
+
     this.dataservice.getData().then((data) => {
-      for(let i = 0; i < 10; i++){
-        if(data[i].fields.gtin_cd === "0033383001531"){
-          // console.log("Product gtin_cd: " + data[i].fields.gtin_cd);
-          // console.log("Brand Name: " + data[i].fields.brand_nm);
-          // console.log("Brand Image: " + data[i].fields.brand_img);
-          // console.log("Brand Link: " + data[i].fields.brand_link);
+      for(let i = 0; i < data.length; i++){
+        if(data[i].fields.gtin_cd === "57063002003"){
+          this.foundIt = true;
+           console.log("Product gtin_cd: " + data[i].fields.gtin_cd);
+          console.log("Brand Name: " + data[i].fields.brand_nm);
+          console.log("Brand Image: " + data[i].fields.brand_img);
+          console.log("Brand Link: " + data[i].fields.brand_link);
           this.data =[
             {
               gtin: data[i].fields.gtin_cd,
@@ -52,11 +69,12 @@ export class ScannPage {
               vitamin_c: data[i].fields.vitamin_c
             }
           ]
+          //this.foundIt = true;
         }
         console.log(data[i]);
       }
-
     });
+    this.getBarcode();
   }
 
 
