2394ed1265e337d753691a417355b07d4dcbe02d
Mark Caruso <kazoo6723@gmail.com>
2017-05-03 21:01:08 -0400

    Added barcode getData() to scann.ts and it is working, added imports along with it. Added InAppBrowser to scann.ts and it is working with launch() function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -59,42 +59,43 @@ export class HomePage {
   	this.navCtrl.push(ScannPage, {details:details});
   }
 
-  launchUrl(url){
-    this.platform.ready().then(() => {
-        this.iab.create( url, "_system","location=yes" );
-    })
-  }
+  // launchUrl(url){
+  //   this.platform.ready().then(() => {
+  //       this.iab.create( url, "_system","location=yes" );
+  //   })
+  // }
 
 
-  ngOnInit(){
-    this.dataService.getData().then((data) => {
-      for(let i = 0; i < 10; i++){
-        if(data[i].fields.gtin_cd === "0033383001531"){
-          // console.log("Product gtin_cd: " + data[i].fields.gtin_cd);
-          // console.log("Brand Name: " + data[i].fields.brand_nm);
-          // console.log("Brand Image: " + data[i].fields.brand_img);
-          // console.log("Brand Link: " + data[i].fields.brand_link);
-          this.data =[
-            {
-              gtin: data[i].fields.gtin_cd,
-              brandname: data[i].fields.brand_nm,
-              gtin_nm:data[i].fields.gtin_nm,
-              img: data[i].fields.brand_img,
-              link: data[i].fields.brand_link,
-              cal: data[i].fields.cal,
-              cal_from_fat: data[i].fields.cal_from_fat,
-              ingredients: data[i].fields.ingredients,
-              protein: data[i].fields.protein_g,
-              sugars: data[i].fields.sugars_g,
-              vitamin_a: data[i].fields.vitamin_a,
-              vitamin_c: data[i].fields.vitamin_c
-            }
-          ]
-        }
-        console.log(data[i]);
-      }
+//   ngOnInit(){
+//     this.dataService.getData().then((data) => {
+//       for(let i = 0; i < 10; i++){
+//         if(data[i].fields.gtin_cd === "0033383001531"){
+//           // console.log("Product gtin_cd: " + data[i].fields.gtin_cd);
+//           // console.log("Brand Name: " + data[i].fields.brand_nm);
+//           // console.log("Brand Image: " + data[i].fields.brand_img);
+//           // console.log("Brand Link: " + data[i].fields.brand_link);
+//           this.data =[
+//             {
+//               gtin: data[i].fields.gtin_cd,
+//               brandname: data[i].fields.brand_nm,
+//               gtin_nm:data[i].fields.gtin_nm,
+//               img: data[i].fields.brand_img,
+//               link: data[i].fields.brand_link,
+//               cal: data[i].fields.cal,
+//               cal_from_fat: data[i].fields.cal_from_fat,
+//               ingredients: data[i].fields.ingredients,
+//               protein: data[i].fields.protein_g,
+//               sugars: data[i].fields.sugars_g,
+//               vitamin_a: data[i].fields.vitamin_a,
+//               vitamin_c: data[i].fields.vitamin_c
+//             }
+//           ]
+//         }
+//         console.log(data[i]);
+//       }
+//
+//     });
+// }
+//End of ngOnInit()
 
-    });
-}
-
-}
+}//End of Class
--- src/pages/scann/scann.ts
+++ src/pages/scann/scann.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { BcodeProvider} from '../../providers/bcode-provider'
+import { BcodeProvider} from '../../providers/bcode-provider';
+import { Platform } from 'ionic-angular';
+import {InAppBrowser } from '@ionic-native/in-app-browser';
 
 
 @Component({
@@ -10,21 +12,51 @@ import { BcodeProvider} from '../../providers/bcode-provider'
 
 export class ScannPage {
 	private barcodeData;
+	data = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              public dataservice: BcodeProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public dataservice: BcodeProvider,
+    public platform: Platform,
+    private iab: InAppBrowser) {
   	 this.barcodeData = this.navParams.get('details');
+  }
 
-  	 //  this.dataservice.load().then((data) => {
-      // console.log(data);
+  launchUrl(url){
+    this.platform.ready().then(() => {
+      this.iab.create( url, "_system","location=yes" );
+    })
+  }
+  ngOnInit(){
+    this.dataservice.getData().then((data) => {
+      for(let i = 0; i < 10; i++){
+        if(data[i].fields.gtin_cd === "0033383001531"){
+          // console.log("Product gtin_cd: " + data[i].fields.gtin_cd);
+          // console.log("Brand Name: " + data[i].fields.brand_nm);
+          // console.log("Brand Image: " + data[i].fields.brand_img);
+          // console.log("Brand Link: " + data[i].fields.brand_link);
+          this.data =[
+            {
+              gtin: data[i].fields.gtin_cd,
+              brandname: data[i].fields.brand_nm,
+              gtin_nm:data[i].fields.gtin_nm,
+              img: data[i].fields.brand_img,
+              link: data[i].fields.brand_link,
+              cal: data[i].fields.cal,
+              cal_from_fat: data[i].fields.cal_from_fat,
+              ingredients: data[i].fields.ingredients,
+              protein: data[i].fields.protein_g,
+              sugars: data[i].fields.sugars_g,
+              vitamin_a: data[i].fields.vitamin_a,
+              vitamin_c: data[i].fields.vitamin_c
+            }
+          ]
+        }
+        console.log(data[i]);
+      }
 
-      // this.description = data.description;
-      // this.productname = data.itemname;
-      // this.avg_price = data.avg_price;
-      // });
-
-
-
+    });
   }
 
 
