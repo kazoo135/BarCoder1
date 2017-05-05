36d043747d0cada8987a99dcbcc651835d7668c1
Mark Caruso <kazoo6723@gmail.com>
2017-05-05 17:30:18 -0400

    Added new component - intro-page to home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Directive} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { ScannPage } from '../scann/scann';
 import { StatusBar } from '@ionic-native/status-bar';
-import { ViewChild } from '@angular/core';
+// import { ViewChild } from '@angular/core';
+import {IntroPage } from '../intro/intro';
+import {componentModuleUrl} from "@angular/compiler";
 
+@Component({
+  selector:'intro-page',
+  templateUrl: '../intro/intro.html'
+})
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -20,30 +26,6 @@ export class HomePage {
   data = [];
   error:any;
 
-  //TODO add an image arry to be used at top of page
-  @ViewChild('slides')slides: any;
-  imgArray = [{
-    title: 'Fresh Vegetables',
-    source: 'assets/images/foods.png',
-    alt: 'Image of Fresh Vegetables'
-  },
-    {
-      title: 'General Products',
-      source: 'assets/images/products1.png',
-      alt: 'Image of various food products on store shelves'
-    },
-    {
-      title: 'More Products',
-      source: 'assets/images/product2.png',
-      alt: 'Image of collection of Food Products'
-
-    },
-    {
-      title: 'Healthy Sancks',
-      source: 'assets/images/products3.png',
-      alt: 'Image of Healthy Snacks'
-    }
-  ]
 
   constructor(
     public navCtrl: NavController, public platform:Platform,
