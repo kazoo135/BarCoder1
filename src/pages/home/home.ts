66cbbdeef94e609bbe66b75fc7a72d779826d1ec
Mark Caruso <kazoo6723@gmail.com>
2017-05-08 12:00:02 -0400

    swapped product2 for products4 img. styled slides component adjusting its height, styled slides images with margin-top and margin-bottom

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,12 @@
-import {Component, Directive} from '@angular/core';
+import {Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { ScannPage } from '../scann/scann';
 import { StatusBar } from '@ionic-native/status-bar';
-// import { ViewChild } from '@angular/core';
-// import {IntroPage } from '../intro/intro';
+import {ViewChild} from '@angular/core';
+import {Slides} from 'ionic-angular';
 
-
-@Component({
-  selector:'page-intro',
-  templateUrl: '../intro/intro.html'
-})
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -26,6 +21,29 @@ export class HomePage {
   data = [];
   error:any;
 
+  @ViewChild('slides')slides:Slides;
+  imgArray = [{
+    title: 'Fresh Vegetables',
+    source: 'assets/images/foods.png',
+    alt: 'Image of Fresh Vegetables'
+  },
+    {
+      title: 'General Products',
+      source: 'assets/images/products1.png',
+      alt: 'Image of various food products on store shelves'
+    },
+    {
+      title: 'More Products',
+      source: 'assets/images/products4.png',
+      alt: 'Image of collection of Food Products'
+
+    },
+    {
+      title: 'Healthy Sancks',
+      source: 'assets/images/products3.png',
+      alt: 'Image of Healthy Snacks'
+    }
+  ]
 
   constructor(
     public navCtrl: NavController, public platform:Platform,
@@ -35,6 +53,7 @@ export class HomePage {
 
   {
   		this.navcontroller = navCtrl;
+
   }
 //  TODO use an ionic hook here instead of ngOnInit()
 ngOnInit(){
